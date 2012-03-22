a679522bf5a27aa6eba8b1a4ffff1391f9e0b20a
Jimmy Theis <jimmy@jetheis.com>
2012-03-22 16:30:44 -0400

    More work
    Signed-off-by: Jimmy Theis <jimmy@jetheis.com>

--- atcpodcast.rb
+++ atcpodcast.rb
@@ -1,7 +1,10 @@
 require 'rubygems'
 require 'bundler/setup'
 
+require 'net/http'
+
 require 'haml'
+require 'json'
 require 'sinatra'
 
 
@@ -11,4 +14,18 @@ $stdout.sync = true
 
 get '/' do
     haml :index
+end
+
+get '/podcast' do
+    request_url = "http://api.npr.org/query?id=2&fields=title,storyDate,audio&dateType=story&output=JSON&numResults=50&apiKey=#{ENV['NPR_API_KEY']}"
+    puts request_url
+    response = Net::HTTP.get_response(URI.parse(request_url))
+    data = response.body
+    result = JSON.parse(data)
+    program = result['list']
+    program['story'].each do |story|
+        puts story['title']['$text']
+    end
+
+    'no error'
 end
